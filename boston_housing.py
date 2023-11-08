65c47640b2987181efc4144e2dd247bd304a0799
Alejandro <13650diaz@gmail.com>
2023-11-08 17:26:25 -0300

    adding in data

--- boston_housing.py
+++ boston_housing.py
@@ -2,8 +2,42 @@
 import pandas as pd
 
 # 1st step is to convert the txt file into a csv
-read_file = pd.read_csv (r'./boston_housing.txt')
-read_file.to_csv (r'./boston_housing.csv', index=None)
+with open('./boston_housing2.txt') as f:
+    # Read space-delimited file and replace all empty spaces by commas
+    data = f.read().replace('\n  ', ',')
+    # Write the CSV data in the output file
+    print(data, file=open('data1.txt', 'w'))
+
+with open('./data1.txt') as f:
+    # Read space-delimited file and replace all empty spaces by commas
+    # data = f.read().replace(' ', ',')
+    # Write the CSV data in the output file
+    line_lst = []
+    for line in f.readlines():
+        # strips leading white spaces
+        strip_white_spaces = line.lstrip()
+        # replaces longer spaces with a single space
+        strip_white_spaces = strip_white_spaces.replace("   ", " ")
+        strip_white_spaces = strip_white_spaces.replace("  ", " ")
+        # replace single white spaces with a comma
+        strip_white_spaces = strip_white_spaces.replace(" ", ",")
+        # fix instances of double comma
+        strip_white_spaces = strip_white_spaces.replace(",,", ",")
+
+        line_lst.append(strip_white_spaces)
+
+    lines = ''.join(line_lst)
+    print(lines, file=open('data2.csv', 'w'))
+
+
+data_frame = pd.read_csv("data2.csv", sep=',',
+                         names=['Crime', 'Zone', 'Indus', 'Chas', 'NOX', 'RM',
+                                'age','dis', 'rad', 'tax', 'ptratio', 'b',
+                                'ltstat', 'medv' ])
+
+data_frame.to_csv('final_data.csv', index=None)
+
+
 
 print("done")
 
